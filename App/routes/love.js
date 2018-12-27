2b22ab3b0b7b421858c1c0657247b30503f14e56
Palmer Krehel <palmerkrehel@gmail.com>
2018-12-27 17:00:43 +0000

    Add favorite functionality with recipe data (id, image, allergens, title)

--- App/routes/love.js
+++ App/routes/love.js
@@ -6,31 +6,25 @@ var User = require("../models/user");
 //Favorite Post
 router.post("/", function (req, res) {
    //lookup recipe using ID
-   Recipe.findByIdAndUpdate(req.params.id, {
-      $addToSet: { 'lovedBy': req.user.id }
-   }, function (err, recipe) {
+   Recipe.findById(req.params.id, function (err, recipe) {
       if (err) {
-         console.log(err);
+         req.flash('error', 'An error has occurred. ' + recipe.title + ' was not added to your favorites.');
          res.redirect("back");
       }
       else {
-         recipe.save();
-         User.findByIdAndUpdate(req.user.id, {
-            $addToSet: {
-               'lovedRecipes': { "id": recipe._id, "title": recipe.title, "image": recipe.image, "createdAt": recipe.createdAt }
-            }
-         }, function (err, foundUser) {
-            if (err) {
-               console.log(err);
-               req.flash('error', 'An error has occurred. ' + recipe.title + ' was not added to your favorites.');
-               res.redirect("back");
-            }
-            else {
-               foundUser.save();
-               req.flash('success', recipe.title, ' has been added to your favorites!');
-               res.redirect('/recipes/' + recipe._id);
-            }
-         });
+         User.findOneAndUpdate(req.user.id, { $addToSet: { 'lovedRecipes': { "_id": recipe.id, "title": recipe.title, "image": recipe.image, "allergens": recipe.allergens } } },
+            function (err, user) {
+               if (err) {
+                  console.log(err);
+                  req.flash("error", "An Error Has Occurred. Please Try Again.");
+                  res.redirect("back");
+               }
+               else {
+                  user.save();
+                  req.flash("success", recipe.title + "was added to your favorites!");
+                  res.redirect("back");
+               }
+            });
       }
    });
 });
