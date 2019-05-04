043e72d1e7de60baee937be7259423eecb9cd81c
Palmer Krehel <palmerkrehel@gmail.com>
2019-05-04 17:15:06 -0400

    limit, sorting and filtering url params

--- App/routes/api.js
+++ App/routes/api.js
@@ -1,38 +1,33 @@
-// var express = require("express"),
-//     router = express.Router(),
-//     Recipe = require("../models/recipe");
-
-// router.get("/v1/Recipes", function (req, res) {
-//     var sort = req.query.sort,
-//         order_by = req.query.order,
-//         limit = req.query.limit
-//     Recipe.find({}).limit(500).exec(function (err, allRecipes) {
-//         if (err) {
-//             res.send(err);
-//         }
-//         else {
-//             res.send(allRecipes)
-//         }
-//     });
-// });
-
-// module.exports = router;
-
-
 var express = require("express"),
     router = express.Router(),
     Recipe = require("../models/recipe");
 
+
+
 router.get("/v1/Recipes", function (req, res) {
-    var sort = req.query.sort,
-        order_by = req.query.order,
-        limit = req.query.limit
-    Recipe.find({}).limit(500).exec(function (err, allRecipes) {
+    var sortBy = req.query.sortBy
+    var orderBy = req.query.orderBy
+    var sort
+    var limit = req.query.limit
+    if(sortBy === '1'){
+        sort = "" + orderBy
+    } else {
+        sort = '-' + orderBy
+    }
+    if(parseInt(limit) <= 500){
+        limit = parseInt(limit)
+    } else{
+        limit = 500
+    }
+    console.log(limit)
+    console.log(sort)
+    Recipe.find()
+    .sort(sort.toString()).limit(limit).exec(function (err, recipes) {
         if (err) {
             res.send(err);
         }
         else {
-            res.send(allRecipes)
+            res.send(recipes)
         }
     });
 });
